16f9533c9dbd158bbc20c6f26f894c4917470087
Andy Pan <panjf2000@gmail.com>
2019-07-20 12:26:44 +0800

    Test on 20/07/2019

--- zhihu/zhihu_me.py
+++ zhihu/zhihu_me.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
         print(f'Refresh statistical data on {today}')
     except Exception as ex:
         writer.writerow([today, 'null', 'null', 'null', 'null', 'null'])
-        exit('Error occurred when calling zhihu api and parsing response!')
+        print('Error occurred when calling zhihu api and parsing response!')
